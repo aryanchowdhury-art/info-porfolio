9d44401387198e901e6c2a2924b7a7071c24eabf
aryanchowdhury-art <aryanchowdhury-art>
2025-02-09 20:24:10 +0530

    deploy react to github

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "info-porfolio",
   plugins: [react()],
-  base: "/Portfolio--react",
 })
